cf7d112597cb39fdb4d6cb7d5d376332da768933
black <yehinerzapater15@gmail.com>
2023-11-24 22:19:58 -0500

    cambios

---
+++ clase1.py
@@ -0,0 +1,13 @@
+import tkinter 
+
+window =tkinter.Tk()
+
+window.title("jugador")
+window.geometry("1420x720")
+
+print("inicio de juego")
+print('jugador numero 1')
+print('pierde ' "cpu")
+print("gana jugador")
+
+window.mainloop()
